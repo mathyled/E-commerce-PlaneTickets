83e2d52792a5ef0758d531243cd7e2ea2ef2998c
CristianV1 <criscvm-29@hotmail.com>
2022-03-31 09:36:05 -0500

    filters added to home

--- client/src/components/Users/Pages/Home/Home.jsx
+++ client/src/components/Users/Pages/Home/Home.jsx
@@ -1,38 +1,38 @@
-
-import React,{useState} from "react";
+import React, { useState } from "react";
 // import {useSelector,useDispatch} from "react-redux";
 // import {Link} from "react-router-dom";
 // import Paged from "../../Features/Paged/Paged";
 import NavBar from "../../Features/NavBar/NavBar";
+import Filters from "../../Features/Filters/Filters";
 
 function Home() {
-    // const tickets = useSelector((state) => state.tickets);
-    // const page = useSelector((state) => state.actualPage);
-    // const dispatch = useDispatch()
-    // const [TicketsPerPage] = useState(20);
+  // const tickets = useSelector((state) => state.tickets);
+  // const page = useSelector((state) => state.actualPage);
+  // const dispatch = useDispatch()
+  // const [TicketsPerPage] = useState(20);
 
-    // const indexOfLastTicket = page * TicketsPerPage;
-    // const indexOfFirstTicket = indexOfLastTicket - TicketsPerPage;
-    // const currentTickets = tickets.slice(indexOfFirstTicket, indexOfLastTicket);
-   
-    // function paginate( numberPage) {
-    //     dispatch(changePage(numberPage));
-    //   }
+  // const indexOfLastTicket = page * TicketsPerPage;
+  // const indexOfFirstTicket = indexOfLastTicket - TicketsPerPage;
+  // const currentTickets = tickets.slice(indexOfFirstTicket, indexOfLastTicket);
 
-    return (
-        <div >
-         
-           <h1>
-             <NavBar /> Home Page
-           </h1>
-      
-            {/* <Paged
+  // function paginate( numberPage) {
+  //     dispatch(changePage(numberPage));
+  //   }
+
+  return (
+    <div>
+      <h1>
+        <NavBar /> Home Page
+        <Filters />
+      </h1>
+
+      {/* <Paged
               TicketsPerPage={TicketsPerPage}
               total={tickets}
               paginate={paginate}
             /> */}
 
-                  {/* {
+      {/* {
                     currentTickets?.map(c => {
                         return (
                             <div key={c.id}>
@@ -45,8 +45,8 @@ function Home() {
                         )
                     })
                 } */}
-        </div>
-    );
+    </div>
+  );
 }
 
 export default Home;
