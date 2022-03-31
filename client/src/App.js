479e4b0029df9c20068b58aa3a469be4590d0527
Manuel Paredes <mapa991@hotmail.com>
2022-03-31 17:58:20 -0500

    change routes for landing

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,19 @@
-import './App.css';
+import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import Home from "./components/Users/Pages/Home/Home";
-import{NotfoundPage} from "./components/Users/Pages/NotfoundPage/NotfoundPage"
-import {ForgotPasswordPage} from "./components/Users/Features/UserModal/pages/ForgotPasswordPage"
+import { NotfoundPage } from "./components/Users/Pages/NotfoundPage/NotfoundPage";
+import { ForgotPasswordPage } from "./components/Users/Features/UserModal/pages/ForgotPasswordPage";
+import LandingPage from "./components/Users/Features/Landing/LandingPage";
 function App() {
   return (
     <div className="App">
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route exact path='/forgot-password' element={<ForgotPasswordPage />} />
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/home" element={<Home />} />
+        <Route exact path="/forgot-password" element={<ForgotPasswordPage />} />
 
-
-        <Route exact path='*' element={<NotfoundPage />} />
+        <Route exact path="*" element={<NotfoundPage />} />
       </Routes>
-
     </div>
   );
 }
