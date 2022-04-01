6b820ac6fa047ac8a94e97384eb1f9446dcc1732
unknown <mathiasimagine@gmail.com>
2022-04-01 12:11:49 -0300

    fix bug commit

--- client/src/App.js
+++ client/src/App.js
@@ -5,17 +5,10 @@ import Home from "./components/Users/Pages/Home/Home";
 import{NotfoundPage} from "./components/Users/Pages/NotfoundPage/NotfoundPage"
 import {ForgotPasswordPage} from "./components/Users/Features/UserModal/pages/ForgotPasswordPage"
 import Profilepage from "./components/Users/Features/UserModal/pages/Profilepage";
-<<<<<<< HEAD
-import {ResetPasswordPage} from "./components/Users/Features/UserModal/pages/ResetPasswordPage"
-import LandingPage from "./components/Users/Features/Landing/LandingPage";
-import ProtectedRoute from "./ProtectedRoutes"
-=======
-
 import ProtectedRoute from "./ProtectedRoutes";
 import {ResetPasswordPage} from "./components/Users/Features/UserModal/pages/ResetPasswordPage"
 import LandingPage from "./components/Users/Features/Landing/LandingPage";
 
->>>>>>> 3dece47cbd627d910ae77497f448e51f777db1f3
 
 function App() {
   return (
