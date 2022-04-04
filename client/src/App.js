ba0223adb9ff2a2cd10ea41ff6dc3965a9b338c2
Manuel Paredes <mapa991@hotmail.com>
2022-04-04 09:40:11 -0500

    add new-fligth endpoint to render Form

--- client/src/App.js
+++ client/src/App.js
@@ -1,38 +1,33 @@
-
-import './App.css';
-import { Routes, Route} from "react-router-dom";
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
 import Home from "./components/Users/Pages/Home/Home";
-import{NotfoundPage} from "./components/Users/Pages/NotfoundPage/NotfoundPage"
-import {ForgotPasswordPage} from "./components/Users/Features/UserModal/pages/ForgotPasswordPage"
+import { NotfoundPage } from "./components/Users/Pages/NotfoundPage/NotfoundPage";
+import { ForgotPasswordPage } from "./components/Users/Features/UserModal/pages/ForgotPasswordPage";
 import Profilepage from "./components/Users/Features/UserModal/pages/Profilepage";
 import ProtectedRoute from "./ProtectedRoutes";
-import {ResetPasswordPage} from "./components/Users/Features/UserModal/pages/ResetPasswordPage"
+import { ResetPasswordPage } from "./components/Users/Features/UserModal/pages/ResetPasswordPage";
 import LandingPage from "./components/Users/Features/Landing/LandingPage";
-
+import CreateForm from "./components/Users/Pages/Create/CreateForm";
 
 function App() {
   return (
     <div className="App">
       <Routes>
         <Route path="/" element={<LandingPage />} />
+        <Route path="/new-flight" element={<CreateForm />} />
         <Route path="/home" element={<Home />} />
         <Route exact path="/forgot-password" element={<ForgotPasswordPage />} />
 
-        <Route element={<ProtectedRoute />}> 
-        <Route exact path='/profile' element={<Profilepage />} />
+        <Route element={<ProtectedRoute />}>
+          <Route exact path="/profile" element={<Profilepage />} />
         </Route>
 
-        <Route exact path='/reset-password' element={<ResetPasswordPage />} />
+        <Route exact path="/reset-password" element={<ResetPasswordPage />} />
 
-        <Route exact path='*' element={<NotfoundPage />} />
-
+        <Route exact path="*" element={<NotfoundPage />} />
       </Routes>
     </div>
   );
 }
-
-
-
-
 
 export default App;
