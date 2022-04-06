27266c216eb621640a147163e7a34ed5d3eb795d
Alexis Campusano <82987189+aleecmp@users.noreply.github.com>
2022-04-06 14:24:43 -0400

    feat add CRUD Itinerary

--- api/src/routes/createFlight.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { getFlightsDb, createFlightDb } = require("../controllers/createFlightDb");
-
-router.get("/", getFlightsDb);
-router.post("/", createFlightDb);
-
-module.exports = router;
