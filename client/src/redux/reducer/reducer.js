4165d20621b1565264da2b927cfd88651588b4cc
unknown <mathiasimagine@gmail.com>
2022-04-06 23:53:41 -0300

    feat redux of Cart

--- client/src/redux/reducer/reducer.js
+++ client/src/redux/reducer/reducer.js
@@ -5,16 +5,39 @@ const initialState = {
   city_details: {},
   cityBackUp: [],
   search: [],
+  products: [ {
+    "_id": "624e4eea4c930e75e9452693",
+ 
+    "departure": {
+      "iataCode": "aep",
+      "icaoCode": "sabe",
+      "scheduledTime": "04:50",
+      "nameCity": "Buenos Aires",
+      "image": "https://www.lugaresturisticos.pro/wp-content/uploads/2019/06/image2-1024x790.png"
+    },
+    "arrival": {
+      "iataCode": "juj",
+      "scheduledTime": "07:05",
+      "nameCity": "Jujuy",
+      "image": "https://www.lugaresturisticos.pro/wp-content/uploads/2019/06/image2-1024x790.png"
+    },
+
+  
+    "codeshared": {
+      "airline": {
+        "name": "aerolineas argentinas (retro livery)",
+      },
+
+    },
+    "price": 372,
+  
+  }],
+  cart: [],
+  currentItem: null,
 };
 
 function rootReducer(state = initialState, action) {
   switch (action.type) {
-    // case TYPES.GET_OFFERS:
-    //   return {
-    //     ...state,
-    //     city: action.payload.data,
-    //     cityBackUp: action.payload.data,
-    //   };
 
     case TYPES.RESET_STATES:
       return {
@@ -34,109 +57,109 @@ function rootReducer(state = initialState, action) {
       if (action.payload.to === true) {
         action.payload.ascending
           ? (sortedData = state.city.sort((a, b) => {
-              if (a.arrival.nameCity === undefined) {
-                return 1;
-              }
-              if (b.arrival.nameCity === undefined) {
-                return -1;
-              }
-              if (a.arrival.nameCity.area === null) {
-                return 1;
-              }
-              if (b.arrival.nameCity === null) {
-                return -1;
-              }
-              return a.arrival.nameCity.charAt(0) < b.arrival.nameCity.charAt(0)
-                ? -1
-                : 1;
-            }))
+            if (a.arrival.nameCity === undefined) {
+              return 1;
+            }
+            if (b.arrival.nameCity === undefined) {
+              return -1;
+            }
+            if (a.arrival.nameCity.area === null) {
+              return 1;
+            }
+            if (b.arrival.nameCity === null) {
+              return -1;
+            }
+            return a.arrival.nameCity.charAt(0) < b.arrival.nameCity.charAt(0)
+              ? -1
+              : 1;
+          }))
           : (sortedData = state.city.sort((a, b) => {
-              if (a.arrival.nameCity === undefined) {
-                return 1;
-              }
-              if (b.arrival.nameCity === undefined) {
-                return -1;
-              }
-              if (a.arrival.nameCity.area === null) {
-                return 1;
-              }
-              if (b.arrival.nameCity === null) {
-                return -1;
-              }
-              return a.arrival.nameCity.charAt(0) > b.arrival.nameCity.charAt(0)
-                ? -1
-                : 1;
-            }));
+            if (a.arrival.nameCity === undefined) {
+              return 1;
+            }
+            if (b.arrival.nameCity === undefined) {
+              return -1;
+            }
+            if (a.arrival.nameCity.area === null) {
+              return 1;
+            }
+            if (b.arrival.nameCity === null) {
+              return -1;
+            }
+            return a.arrival.nameCity.charAt(0) > b.arrival.nameCity.charAt(0)
+              ? -1
+              : 1;
+          }));
       }
       if (action.payload.price === true) {
         action.payload.ascending
           ? (sortedData = state.city.sort((a, b) => {
-              if (a.price === undefined) {
-                return 1;
-              }
-              if (b.price === undefined) {
-                return -1;
-              }
-              if (a.price === null) {
-                return 1;
-              }
-              if (b.price === null) {
-                return -1;
-              }
-              return parseFloat(a.price) < parseFloat(b.price) ? -1 : 1;
-            }))
+            if (a.price === undefined) {
+              return 1;
+            }
+            if (b.price === undefined) {
+              return -1;
+            }
+            if (a.price === null) {
+              return 1;
+            }
+            if (b.price === null) {
+              return -1;
+            }
+            return parseFloat(a.price) < parseFloat(b.price) ? -1 : 1;
+          }))
           : (sortedData = state.city.sort((a, b) => {
-              if (a.price === undefined) {
-                return 1;
-              }
-              if (b.price === undefined) {
-                return -1;
-              }
-              if (a.price === null) {
-                return 1;
-              }
-              if (b.price === null) {
-                return -1;
-              }
-              return parseFloat(a.price) > parseFloat(b.price) ? -1 : 1;
-            }));
+            if (a.price === undefined) {
+              return 1;
+            }
+            if (b.price === undefined) {
+              return -1;
+            }
+            if (a.price === null) {
+              return 1;
+            }
+            if (b.price === null) {
+              return -1;
+            }
+            return parseFloat(a.price) > parseFloat(b.price) ? -1 : 1;
+          }));
       }
       if (action.payload.schedule != "") {
         action.payload.ascending
           ? state.city.sort((a, b) => {
-              if (a.departure.scheduledTime === undefined) {
-                return 1;
-              }
-              if (b.departure.scheduledTime === undefined) {
-                return -1;
-              }
-              if (a.departure.scheduledTimee === null) {
-                return 1;
-              }
-              if (b.departure.scheduledTime === null) {
-                return -1;
-              }
-              return a.departure.scheduledTime < b.price.departure.scheduledTime
-                ? -1
-                : 1;
-            })
+            if (a.departure.scheduledTime === undefined) {
+              return 1;
+            }
+            if (b.departure.scheduledTime === undefined) {
+              return -1;
+            }
+            if (a.departure.scheduledTimee === null) {
+              return 1;
+            }
+            if (b.departure.scheduledTime === null) {
+              return -1;
+            }
+            return a.departure.scheduledTime < b.price.departure.scheduledTime
+              ? -1
+              : 1;
+          })
           : state.city.sort((a, b) => {
-              if (a.departure.scheduledTime === undefined) {
-                return 1;
-              }
-              if (b.departure.scheduledTime === undefined) {
-                return -1;
-              }
-              if (a.departure.scheduledTime === null) {
-                return 1;
-              }
-              if (b.departure.scheduledTime === null) {
-                return -1;
-              }
-              return a.departure.scheduledTime > b.price.departure.scheduledTime
-                ? -1
-                : 1;
-            });
+            if (a.departure.scheduledTime === undefined) {
+              return 1;
+            }
+            if (b.departure.scheduledTime === undefined) {
+              return -1;
+            }
+            if (a.departure.scheduledTime === null) {
+              return 1;
+            }
+            if (b.departure.scheduledTime === null) {
+              return -1;
+            }
+            return a.departure.scheduledTime > b.price.departure.scheduledTime
+              ? -1
+              : 1;
+          });
       }
       return { ...state, city: [...sortedData] };
     case TYPES.FILTER_CITIES:
@@ -161,12 +184,6 @@ function rootReducer(state = initialState, action) {
       }*/
       return { ...state };
 
-    // case TYPES.GET_CITIES:
-    //   return {
-    //     ...state,
-    //     city: action.payload.data,
-    //     cityBackUp: action.payload.data,
-    //   };
     case TYPES.GET_FLIGHTS:
       return {
         ...state,
@@ -184,6 +201,16 @@ function rootReducer(state = initialState, action) {
         ...state,
       };
 
+    //// CART ////////////
+
+    case TYPES.ADD_TO_CART:
+      return {}
+      case TYPES.REMOVE_FROM_CART:
+        return {}
+    case TYPES.ADD_QUANTITY:
+      return {}
+    case TYPES.LOAD_CURRENT_ITEM:
+      return {}
     default:
       return { ...state };
   }
