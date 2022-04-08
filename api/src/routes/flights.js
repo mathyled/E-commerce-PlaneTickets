d2cd5460be623d5698fd8fa006beb84da7b7fb02
EzeJuarez <eze-juarez@live.com>
2022-04-08 12:50:05 -0300

    feat crud flights and add autocomplete search

--- api/src/routes/flights.js
+++ api/src/routes/flights.js
@@ -1,8 +1,14 @@
 const express = require("express");
-const { getFlights, getFlightDetail } = require("../controllers/flights");
+const { getFlights, getFlightDetail, deleteFlight } = require("../controllers/flights");
 const router = express.Router();
+// const {
+//     verifyToken,
+//     verifyTokenAndAuthorization,
+//     verifyTokenAndAdmin,
+// } = require("./verifyToken");
 
-router.get("/", getFlights); // ej: http://localhost:3001/api/flights?city=AEP&date=2022-04-15
+router.get("/", getFlights); // ej: http://localhost:3001/api/flights?city={AEP}&date={2022-04-15}
 router.get("/detail/:id", getFlightDetail);
+router.delete("/delete/:id", deleteFlight);
 
 module.exports = router;
