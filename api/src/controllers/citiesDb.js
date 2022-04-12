461e8b0e4b9a595a8e1ceb04425fdef3c96b683c
EzeJuarez <eze-juarez@live.com>
2022-04-11 22:33:58 -0300

    feat sort cities

--- api/src/controllers/citiesDb.js
+++ api/src/controllers/citiesDb.js
@@ -2,6 +2,16 @@ const models = require("./models/index");
 
 const getAllCities = (req, res) => {
     const citiesData = models.getCities();
+    citiesData.sort((a, b) => {
+      if(a.nameCity < b.nameCity) {
+        return - 1;
+      };
+      if(a.nameCity > b.nameCity) {
+        return 1;
+      };
+      return 0;
+    });
+    console.log(citiesData);
     res.status(200).send({ message: "success", data: citiesData });
 };
 
