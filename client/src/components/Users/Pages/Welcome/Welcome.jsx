f9a18a031b894f2bf557f265a074e4b97b2e78b4
unknown <mathiasimagine@gmail.com>
2022-04-14 13:24:11 -0300

    feat add SuccessConfirm && fix console error of LandingPage

--- client/src/components/Users/Pages/Welcome/Welcome.jsx
+++ client/src/components/Users/Pages/Welcome/Welcome.jsx
@@ -1,30 +1,30 @@
-import React from "react";
-import axios from "axios";
+// import React from "react";
+// import axios from "axios";
 
-const Welcome = (props) => {
-  const API_URL = "http://localhost:3001/api/auth/";
+// const Welcome = (props) => {
+//   const API_URL = "http://localhost:3001/api/auth/";
 
-  const verifyUser = (code) => {
-    return axios.get(API_URL + "confirm/" + code).then((response) => {
-      return response.data;
-    });
-  };
+//   const verifyUser = (code) => {
+//     return axios.get(API_URL + "confirm/" + code).then((response) => {
+//       return response.data;
+//     });
+//   };
 
-  if (props.match.path === "/confirm/:confirmationCode") {
-    verifyUser(props.match.params.confirmationCode);
-  }
-  return (
-    <div>
-      <header>
-        <h3>
-          <strong>Account confirmed!</strong>
-        </h3>
-      </header>
-      {/* <Link to={"/login"}">
-        Please Login
-      </Link> */}
-    </div>
-  );
-};
+//   if (props.match.path === "/confirm/:confirmationCode") {
+//     verifyUser(props.match.params.confirmationCode);
+//   }
+//   return (
+//     <div>
+//       <header>
+//         <h3>
+//           <strong>Account confirmed!</strong>
+//         </h3>
+//       </header>
+//       {/* <Link to={"/login"}">
+//         Please Login
+//       </Link> */}
+//     </div>
+//   );
+// };
 
-export default Welcome;
+// export default Welcome;
