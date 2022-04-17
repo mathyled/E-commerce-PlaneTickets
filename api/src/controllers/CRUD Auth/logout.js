993576a37a5e13ad093097026f2771402195ed77
Alexis Campusano <82987189+aleecmp@users.noreply.github.com>
2022-04-16 21:04:11 -0400

    Feat add googleLOgin and Logout

--- api/src/controllers/CRUD Auth/logout.js
+++ api/src/controllers/CRUD Auth/logout.js
@@ -1,29 +1,9 @@
-// const { UserModel } = require("../../models/");
+const logout = (req, res) => {
+  req.session.destroy(() => {
+    res.status(200).send({ message: "Logged out" });
+  });
+};
 
-// const logout = async (req, res) => {
-//   try {
-//     req.session.destroy();
-//     res.status(200).send({
-//       message: "Logout successful",
-//     });
-//   } catch (error) {
-//     res.status(400).send({
-//       message: "Logout failed",
-//     });
-//   }
-// };
-
-// const changePassword = async (req, res) => {
-//   try {
-//     // recibo el nuevo password
-//     const { password } = req.body;
-//     // busco el usuario
-//     const user = await UserModel.findById(req.user._id);
-//     // cambio el password
-//     if (!user) {
-//       res.status(400).send({
-//         message: "User not found",
-//       });
-//     } else {
-
-// };
+module.exports = {
+  logout,
+};
--- api/src/routes/auth.js
+++ api/src/routes/auth.js
@@ -1,16 +1,32 @@
 const express = require("express");
 const router = express.Router();
+passport = require("passport");
+require("../middlewares/google");
 
 const { register } = require("../controllers/CRUD Auth/register");
 const { login } = require("../controllers/CRUD Auth/login");
 const { verifyUser } = require("../controllers/CRUD Auth/verifyUser");
 const { forgotPass } = require("../controllers/CRUD Auth/forgotPass");
 const { recoverPass } = require("../controllers/CRUD Auth/recoverPass");
+const { logout } = require("../controllers/CRUD Auth/logout");
+const CLIENT_URL = "http://localhost:3000/home";
 
 router.post("/register", register);
 router.post("/login", login);
 router.get("/confirm/:confirmationCode", verifyUser);
 router.post("/recover", forgotPass);
 router.put("/recover/:token", recoverPass);
+router.get(
+  "/google",
+  passport.authenticate("google", { scope: ["profile", "email"] })
+);
+router.get(
+  "/google/callback",
+  passport.authenticate("google", {
+    successRedirect: CLIENT_URL,
+    failureRedirect: "/login/failed",
+  })
+);
+router.get("/logout", logout);
 
 module.exports = router;
