b1916ca9f820d5037f5ac10c9fcf5f994b5589a8
EzeJuarez <eze-juarez@live.com>
2022-04-19 00:59:31 -0300

    feat add username in orders

--- api/src/models/Order.js
+++ api/src/models/Order.js
@@ -4,6 +4,7 @@ const OrderSchema = new mongoose.Schema(
   {
     userId: { type: String, required: true },
     products: { type: Array },
+    username: { type: String },
     amount: { type: Number, required: true },
     // address: { type: Object, required: true },
     status: { type: String, default: "pending" },
