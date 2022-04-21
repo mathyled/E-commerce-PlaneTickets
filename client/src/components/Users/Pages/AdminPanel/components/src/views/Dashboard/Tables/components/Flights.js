9b8f4c45f0be37550d625897ac1a794d7ee52f3a
Manuel Paredes <mapa991@hotmail.com>
2022-04-21 00:17:47 -0500

    feat maps the data brought from backend to render each row of flights

--- client/src/components/Users/Pages/AdminPanel/components/src/views/Dashboard/Tables/components/Flights.js
+++ client/src/components/Users/Pages/AdminPanel/components/src/views/Dashboard/Tables/components/Flights.js
@@ -9,16 +9,23 @@ import {
   Tr,
   useColorModeValue,
 } from "@chakra-ui/react";
-import React from "react";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { getFlightsAdmin } from "../../../../../../../../../../redux/actions/actions";
 import Card from "../../../../components/Card/Card";
 import CardBody from "../../../../components/Card/CardBody";
 import CardHeader from "../../../../components/Card/CardHeader";
 import CreateForm from "../../../../components/CRUD/CreateForm";
 import FligthsRow from "../../../../components/Tables/FligthsRow";
 
-const Flights = ({ title, captions, data }) => {
+const Flights = ({ title, captions }) => {
   const textColor = useColorModeValue("gray.700", "white");
-
+  const dispatch = useDispatch();
+  const data = useSelector((state) => state.flights_Cart);
+  useEffect(() => {
+    dispatch(getFlightsAdmin());
+  }, [dispatch]);
+  // console.log("La info que llega de back", data);
   return (
     <Card my="22px" overflowX={{ sm: "scroll", xl: "hidden" }}>
       <CardHeader p="6px 0px 22px 0px">
@@ -32,12 +39,17 @@ const Flights = ({ title, captions, data }) => {
         <Box>
           <CreateForm />
           <CardHeader p="6px 0px 22px 0px">
-        <Flex direction="column">
-          <Text fontSize="lg" color={textColor} fontWeight="bold" pb=".5rem">
-            Flights table
-          </Text>
-        </Flex>
-      </CardHeader>
+            <Flex direction="column">
+              <Text
+                fontSize="lg"
+                color={textColor}
+                fontWeight="bold"
+                pb=".5rem"
+              >
+                Flights table
+              </Text>
+            </Flex>
+          </CardHeader>
           <Table variant="simple" color={textColor}>
             <Thead>
               <Tr my=".8rem" pl="0px" color="gray.400">
@@ -55,7 +67,17 @@ const Flights = ({ title, captions, data }) => {
               </Tr>
             </Thead>
             <Tbody>
-              <FligthsRow/>
+              {data?.map((el, i) => (
+                <FligthsRow
+                  key={el._id}
+                  index={i}
+                  depature={el.departure.iataCode}
+                  arrival={el.arrival.iataCode}
+                  price={el.price}
+                  date={el.date}
+                  id={el._id}
+                />
+              ))}
             </Tbody>
           </Table>
         </Box>
